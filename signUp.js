5c843af8715992062b016c3fdc3d2b51d581ffda
Phạm Hồ Hữu Trí <oopscreeper2006@gmail.com>
2022-01-02 11:38:19 +0700

    upadte

--- signUp.js
+++ signUp.js
@@ -1,2 +1,17 @@
+// Khởi tạo list account
+let lstAccount = JSON.parse(localStorage.getItem('lstAccount'));
+console.log('data',lstAccount);
+// Lưu dữ liệu vô local storage
+const elm_signup = document.getElementById('btn');
+elm_signup.addEventListener('click',saveinfo)
 
-
+function saveinfo(){
+    const elm_name = document.getElementById('name');
+    const elm_pw = document.getElementById('pw');
+    const newAccount = {
+        name: elm_name.value,
+        pw: elm_pw.value
+    }
+    lstAccount.push(newAccount);
+    localStorage.setItem('lstAccount', JSON.stringify(lstAccount));
+}
